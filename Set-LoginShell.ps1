a3736ae6536beb14bd03657244e065e06a9153c3
Felipe Matheus de Souza Santos <felipe-santos@outlook.com>
2018-11-14 11:05:12 +0100

    Add files via upload

---
+++ Set-LoginShell.ps1
@@ -0,0 +1,59 @@
+﻿function Set-LoginShell {
+
+<#
+    .SYNOPSIS
+       Quickly set loginshell attribute for an AD user.
+    
+    .DESCRIPTION
+       Use this script when a person can't use sudo commands or any commands at all into their dev vm's. 
+    
+    .PARAMETER UserName
+        Set the AD user target.
+    
+    .PARAMETER Shell
+        Set the LoginShell attribute.
+    
+    .Example
+        Set-LoginShell -UserName fdesouzasantos -Shell /bin/bash
+        This will set fdesouzasantos loginshell attribute value to /bin/bash             
+#>
+
+    [CmdletBinding(ConfirmImpact = 'High', SupportsShouldProcess)]
+    
+    param(
+    
+        [Parameter(Mandatory)]
+        $UserName,
+        [Parameter(Mandatory)]
+        [ValidateSet("/bin/bash","/bin/zsh")]
+        $Shell
+    )
+
+    $VerbosePreference = 'Continue'
+    
+    Try {
+    
+        $UserData = Get-ADUser -Filter "SamAccountName -eq '$UserName'" -Properties loginshell
+    } 
+    
+    catch {
+    
+        throw "Could not find username $UserName"
+        break;
+    
+    }
+
+        if($UserData.LoginShell -notmatch $Shell) {
+    
+                if($PSCmdlet.ShouldProcess($UserData.DistinguishedName)) {                     
+    
+                    Set-Aduser -Identity $UserData -Replace @{loginshell=$Shell}     
+                    Write-Verbose "LoginShell attribute for $UserName has been set to: $Shell"
+            
+                }
+
+                else { Write-Verbose "Operation cancelled." }
+        }
+
+        else { Write-Warning "The LoginShell attribute for $($UserData.Name) is already set to $Shell" }
+}  
