7669e0b164523c1d6068f91e4225e76e212d484a
Felipe Matheus de Souza Santos <felipe-santos@outlook.com>
2019-04-13 12:26:43 +0200

    Update Read-RamUsage.ps1

--- Read-RamUsage.ps1
+++ Read-RamUsage.ps1
@@ -26,7 +26,7 @@ function Read-RamUsage {
     param(
         [ValidateScript({Test-Connection $_ -Quiet -Count 1})]                
         [string]$ComputerName = 'localhost',
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory, HelpMessage="Specify RAM check interval in seconds)]
         [int]$Interval,
         [Parameter(Mandatory)]
         [int]$Count
