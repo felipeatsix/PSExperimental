470321e6be10c249c2a7889a263671274ca2b1df
Felipe de Souza Santos <felipe-santos@outlook.com>
2020-07-27 22:39:45 +0200

    update

--- Find-Software.ps1
+++ Find-Software.ps1
@@ -1,115 +1,92 @@
 ﻿Function Find-Software {
     <#
-        .SYNOPSIS
-            Query and find specific software in local or remote computer.
+    .SYNOPSIS
+        Query and find specific software in local or remote computer.
+
+    .DESCRIPTION
+        This Powershell function uses registry path to query all software installed in a computer.
+        This works much better and faster than using win32_product WMI class. 
+
+    .PARAMETER DisplayName
+        Set the name of the software to search for.
+
+    .PARAMETER Publisher
+        Filter the search by Publisher name.
         
-        .DESCRIPTION
-            This Powershell function uses registry path to query all software installed in a computer.
-            This works much better and faster than using win32_product WMI class. 
+    .PARAMETER ComputerName
+        Set the computer name target to run it remotely.
+
+    .EXAMPLE
+        Find-Software -DisplayName '[software]' -ComputerName [hostname]
+        This command will get all installed softwares in [hostname] that matches 'Adwords' keyword in it's name.
+
+        * You can use the alias -Name instead -DisplayName
+
+    .EXAMPLE
+        Find-Software -Software 'Skype'
+        This command will get the software called 'Skype' in the local computer.
         
-        .PARAMETER DisplayName
-            Set the name of the software to search for.
-        
-        .PARAMETER Publisher
-            Filter the search by Publisher name.
-            
-        .PARAMETER ComputerName
-            Set the computer name target to run it remotely.
-        
-        .EXAMPLE
-            Find-Software -DisplayName '[software]' -ComputerName [hostname]
-            This command will get all installed softwares in [hostname] that matches 'Adwords' keyword in it's name.
-    
-            * You can use the alias -Name instead -DisplayName
-        
-        .EXAMPLE
-            Find-Software -Software 'Skype'
-            This command will get the software called 'Skype' in the local computer.
-            
-        .EXAMPLE
-            Find-Software -Publisher 'Microsoft' -ComputerName [hostname]
-            This command will get every software in hostname that have Microsoft as it's publisher.         
-    
-            * You can use the alias -Vendor instead -Publisher
-    #>    
-   
+    .EXAMPLE
+        Find-Software -Publisher 'Microsoft' -ComputerName [hostname]
+        This command will get every software in hostname that have Microsoft as it's publisher.         
+
+        * You can use the alias -Vendor instead -Publisher
+    #>
     [CmdletBinding()]
     Param
     (
-        [Parameter(ValueFromPipelineByPropertyName=$true,
-                    Position=0)]
+        [Parameter(ValueFromPipelineByPropertyName = $true, Position = 0)]
         [Alias('Name')]    
         [String]$DisplayName,
           
-        [Parameter(ValueFromPipelineByPropertyName=$true,
-                    Position=1)]
+        [Parameter(ValueFromPipelineByPropertyName = $true,
+            Position = 1)]
         [Alias('Vendor')]
         [String]$Publisher,
         $ComputerName = 'localhost'
-    )
-        
-    Invoke-Command -ComputerName $ComputerName -ArgumentList $DisplayName,$Publisher,$ComputerName -ScriptBlock {
-    param($DisplayName, $Publisher)
-    
-    #region Filter Prep
-    
-    $whereFilter = '![System.String]::IsNullOrEmpty($_.DisplayName)'
-    Write-Verbose -Message "Base Where filter: $whereFilter"
-        
-        If ($DisplayName) {
-           
-            $whereFilter = '{0} -and ($_.DisplayName -like "{1}")' -f $whereFilter,$DisplayName
+    )        
+    Invoke-Command -ComputerName $ComputerName -ArgumentList $DisplayName, $Publisher, $ComputerName -ScriptBlock {
+        param($DisplayName, $Publisher)    
+        #region Filter Prep    
+        $whereFilter = '![System.String]::IsNullOrEmpty($_.DisplayName)'
+        Write-Verbose -Message "Base Where filter: $whereFilter"        
+        If ($DisplayName) {           
+            $whereFilter = '{0} -and ($_.DisplayName -like "{1}")' -f $whereFilter, $DisplayName
             Write-Verbose -Message "Adding display filter: $whereFilter"
-        }
-        
-        If ($Publisher) {
-       
-            $whereFilter = '{0} -and ($_.Publisher -like "{1}")' -f $whereFilter,$Publisher
+        }        
+        If ($Publisher) {    
+            $whereFilter = '{0} -and ($_.Publisher -like "{1}")' -f $whereFilter, $Publisher
             Write-Verbose -Message "Adding publisher filter: $whereFilter"
-        }
-        
-    $whereBlock = [scriptblock]::Create($whereFilter)
-    
-    #endregion Filter Prep
-        
-    #region Uninstall Registry Keys
-    
-    $UNINSTALL_ROOT = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    $UNINSTALL_WOW = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    $UNINSTALL_USER = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    $UNINSTALL_WOWUSER = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-
-    #endregion Uninstall Registry Keys
-                   
-        $regPath = @{ Name='RegistryKey';Expression={$_.PSPath.Split('::')[-1]} }
-    
+        }        
+        $whereBlock = [scriptblock]::Create($whereFilter)    
+        #endregion Filter Prep    
+        #region Uninstall Registry Keys    
+        $UNINSTALL_ROOT = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        $UNINSTALL_WOW = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        $UNINSTALL_USER = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        $UNINSTALL_WOWUSER = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+        #endregion Uninstall Registry Keys                   
+        $regPath = @{ Name = 'RegistryKey'; Expression = { $_.PSPath.Split('::')[-1] } }    
         [System.String[]]$RegKeys = @(
             $UNINSTALL_ROOT, 
             $UNINSTALL_USER, 
             $UNINSTALL_WOW, 
             $UNINSTALL_WOWUSER
-        )
-    
-        ForEach ($key in $RegKeys) {
-      
-            If (Test-Path -Path $key) {
-            
+        )    
+        ForEach ($key in $RegKeys) {      
+            If (Test-Path -Path $key) {            
                 Write-Verbose -Message "Reading $key"
-                $regKeyColumn = @{ Name='RegistyKey';Expression={$key} }
-            
-                Try {
-                
+                $regKeyColumn = @{ Name = 'RegistyKey'; Expression = { $key } }            
+                Try {                
                     Get-ItemProperty -Path $key  -Name *  -ErrorAction 'Stop' | 
-                    Where-Object $whereBlock | 
-                
+                    Where-Object $whereBlock |                 
                     Select-Object -Property `
-                    DisplayName,
+                        DisplayName,
                     DisplayVersion,
                     Publisher,
                     UninstallString,
                     $regPath
-                }
-            
+                }            
                 Catch { Write-Warning -Message $_.Exception.Message }
             }
         }
--- Read-RamUsage.ps1
+++ Read-RamUsage.ps1
@@ -1,86 +1,92 @@
+#requires -module PSEmoji
 function Read-RamUsage { 
-<#
+    <#
     .SYNOPSIS
         Monitor live RAM usage in local or remote computer through Powershell. 
-    
+
     .DESCRIPTION
         Use this function to monitor RAM usage of a server or an endpoint.
         This can be used to evaluate when a user asks for upgrading RAM.
         Use Ctrl + C to stop it.
-    
+
     .PARAMETER ComputerName
         Set the target computer name.
-    
+
     .PARAMETER Interval
         Set RAM usage check interval.
-    
+
     .PARAMETER Count
         Set the number of RAM checks to do.
-    
+
     .Example
         Read-RamUsage -ComputerName Host01 -Interval 30 -Count 10
         This command will start monitoring Host01 RAM usage
         and will check it 10 times every 30 seconds
         Use Ctrl + C to stop it.
-#>            
+    #>            
     param(
-        [ValidateScript({Test-Connection $_ -Quiet -Count 1})]                
+        [Parameter(Mandatory = $false)]
+        [ValidateScript( {Test-Connection $_ -Quiet -Count 1 })]
         [string]$ComputerName = 'localhost',
-        [Parameter(Mandatory, HelpMessage="Specify RAM check interval in seconds")]
+
+        [Parameter(Mandatory, HelpMessage = "Specify RAM check interval in seconds")]
         [int]$Interval,
-        [Parameter(Mandatory)]
-        [int]$Count
+
+        [Parameter(Mandatory = $true)]
+        [int]$Count,
+
+        [Parameter(Mandatory = $false)]
+        [pscredential]$Credential
     )
-    
-    try{
-    
-        Invoke-Command -ComputerName $ComputerName -ScriptBlock {                        
-            
-            for ($loop = 1 ; $loop -le $Using:Count ; $loop++) {
-                
-                $date = get-date -Format "dd/MM/yyyy - HH:mm:ss" 
-                Write-Output "`nCheck: $loop - Time: $date"
-
-                # Calculate the free RAM percentage
-                # Then set the variables Status and Color depending in it's result.
-                
-                $Ram = Get-WmiObject -class Win32_OperatingSystem
-                $RamFreePercentage = [math]::Round(($ram.FreePhysicalMemory / $ram.TotalVisibleMemorySize) * 100)            
-                                                     
-                if ($RamFreePercentage -gt 45) {
-                                                $Status = "OK"
-                                                $Color = 'Green' 
-                } 
-                
-                elseif ($RamFreePercentage -ge 25) {
-                                                    $Status = "Warning"
-                                                    $Color = 'Yellow'
+    BEGIN {                       
+        $invokeCommand = @{
+            ComputerName = $ComputerName            
+        }
+        if ($Credential) {
+            $invokeCommand.Credential = $Credential
+        }        
+    }    
+    PROCESS {
+        try {    
+            Invoke-Command @invokeCommand -scriptblock {
+                import-module PSEmoji
+                $output = [pscustomobject]@{
+                    'Computer'      = $ENV:COMPUTERNAME
+                    'Total Ram(GB)' = $null
+                    'Free Ram(GB)'  = $null
+                    'Free Ram(%)'   = $null
+                    'Status'        = $null
                 }
-                
-                elseif ($RamFreePercentage -lt 25) {
-                                                    $Status = "Critical"
-                                                    $Color ='Red'
+                for ($loop = 1 ; $loop -le $Using:Count ; $loop++) {
+                    $date = get-date -Format "dd/MM/yyyy - HH:mm:ss" 
+                    Write-Output "Time: $date"
+                    # Calculate the free RAM percentage
+                    # Then set the variables Status and Color depending in it's result.                
+                    $Ram = Get-WmiObject -class Win32_OperatingSystem
+                    $RamFreePercentage = [math]::Round(($ram.FreePhysicalMemory / $ram.TotalVisibleMemorySize) * 100)                                                                
+                    if ($RamFreePercentage -gt 45) {
+                        $Status = $PSEMOJI.Emojis.tests.passed_boxed
+                    }             
+                    elseif ($RamFreePercentage -ge 25) {
+                        $Status = $PSEMOJI.Emojis.face.freezing
+                    }            
+                    elseif ($RamFreePercentage -lt 25) {
+                        $Status = $PSEMOJI.Emojis.tests.bomb
+                    }            
+                    # Get the Total RAM and total free ram
+                    # Then calculate it to be displayed in giga bytes.                
+                    [int]$TotalRamGB = $Ram.TotalVisibleMemorySize / 1mb
+                    [float]$FreeRamGB = [math]::Round($ram.FreePhysicalMemory / 1mb, 2)                                           
+                    $output.'Total Ram(GB)' = $TotalRamGB
+                    $output.'Free Ram(GB)'  = $FreeRamGB
+                    $output.'Free Ram(%)'   = $RamFreePercentage
+                    $output.'Status'        = $Status
+                    Start-Sleep $using:Interval
+                    $output | Out-String
                 }
-                
-                # Get the Total RAM and total free ram
-                # Then calculate it to be displayed in giga bytes.
-                
-                [int]$TotalRamGB = $Ram.TotalVisibleMemorySize / 1mb
-                [float]$FreeRamGB = [math]::Round($ram.FreePhysicalMemory / 1mb,2)                            
-
-                # Output custom object
-                
-                $Ram | Select-Object `
-                @{ Name = 'Computer' ; Expression = {(hostname)}},
-                @{ Name = 'Total Ram(GB)' ; Expression = {$TotalRamGB}},
-                @{ Name = 'Free Ram(GB)' ; Expression = {$FreeRamGB}},
-                @{ Name = 'Free Ram (%)' ; Expression = {"$($RamFreePercentage)%"}},
-                @{ Name = 'Status' ; Expression = {$Status}} |                                
-                Format-Table -AutoSize -Wrap | Out-String | Write-Host -ForegroundColor $Color
-               
-                Start-Sleep $Using:Interval
             }
-        }                
-    } 
+        } 
         catch { Write-Error "$($_.Exception.Message) -Line Number $($_.InvocationInfo.ScriptLineNumber)" }    
+        }    
+    END {}
 }
